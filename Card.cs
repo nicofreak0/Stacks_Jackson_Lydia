d07d5350f4e94b3af9a9e2c8622419d2ec0c9397
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-09-13 13:16:09 -0500

    Initial commit

---
+++ Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks_Jackson_Lydia
+{
+    internal class Card
+    {
+        public string Number { get; }
+
+        public string Suit {  get; }
+
+        public Card(string numParam, string suitParam)
+        {
+            Number = numParam;
+            Suit = suitParam;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks_Jackson_Lydia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card[] myCardsArray = new Card[]
+            {
+                new Card("Ace", "Spades"),
+                new Card("Jack", "Diamonds"),
+                new Card("Queen", "Clubs"),
+                new Card("King", "Hearts"),
+                new Card("8", "Spades"),
+                new Card("4", "Hearts"),
+                new Card("5", "Diamonds"),
+                new Card("2", "Spades"),
+                new Card("7", "Spades"),
+            };
+
+            Stack<Card> startingDeck = new Stack<Card>(myCardsArray);
+
+            Console.WriteLine("Cards in Deck:");
+
+            foreach (Card card in startingDeck)
+            {
+                Console.WriteLine($"The {card.Number} of {card.Suit}.");
+            }
+        }
+    }
+}
