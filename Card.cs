7d22c953dddc4545e7c18702326775f280376094
Lydia <146027704+nicofreak0@users.noreply.github.com>
2024-09-13 14:38:42 -0500

    comments

--- Card.cs
+++ Card.cs
@@ -8,12 +8,16 @@ namespace Stacks_Jackson_Lydia
 {
     internal class Card
     {
+        // property to get the number of the card
         public string Number { get; }
 
+        // property to get the suit of the card
         public string Suit {  get; }
 
+        // constructor to initialize a new card object with specified number and suit
         public Card(string numParam, string suitParam)
         {
+            // assigns a parameter to number and suit
             Number = numParam;
             Suit = suitParam;
         }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Stacks_Jackson_Lydia
     {
         static void Main(string[] args)
         {
+            //an array of card objects with their card names
             Card[] myCardsArray = new Card[]
             {
                 new Card("Ace", "Spades"),
@@ -20,14 +21,52 @@ namespace Stacks_Jackson_Lydia
                 new Card("7", "Spades"),
             };
 
+            // creates a stack card from the array of card objects
             Stack<Card> startingDeck = new Stack<Card>(myCardsArray);
 
+            //output the cards in the deck 
             Console.WriteLine("Cards in Deck:");
 
+            //goes through each card in the stack and prints its details
             foreach (Card card in startingDeck)
             {
                 Console.WriteLine($"The {card.Number} of {card.Suit}.");
             }
+
+            // display the numbers of cards in the deck
+            Console.WriteLine($"Cards in the Deck: {startingDeck.Count}");
+
+            //Create a List<Card> to represent a hand of cards
+            List<Card> myHand = new List<Card>();
+            myHand.Add(new Card("Jack", "Clubs"));
+            myHand.Add(new Card("9", "Spades"));
+            myHand.Add(new Card("Ace", "Clubs"));
+
+            //Check if the deck has any cards before popping one to add to the hand
+            Console.WriteLine("Got Any 8'?");
+            if(startingDeck.Count > 0)
+            {
+                // adds top card from the deck to hand
+                myHand.Add(startingDeck.Pop());
+            }
+
+            // display the updated number of cards in deck
+            Console.WriteLine($"Cards in the Deck: {startingDeck.Count}");
+
+            //return a card from the hand to the deck and remove it from hand
+            startingDeck.Push(myHand[0]);
+            myHand.RemoveAt(0);
+
+            // Output the remaining cards
+            foreach (Card card in startingDeck)
+            {
+                Console.WriteLine($"The {card.Number} of {card.Suit}.");
+            }
+
+           
+
+
+
         }
     }
 }
